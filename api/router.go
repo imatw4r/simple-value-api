157d661ba37fdc636e13ae2b4d1653889eb0283b
Bartek Krzys <bartek@mergedup.com>
2024-01-10 17:45:26 +0100

    ✨ Add API to get Index by Value.

---
+++ api/router.go
@@ -0,0 +1,26 @@
+package api
+
+import (
+	"context"
+	"value-app/config"
+	"value-app/domain"
+
+	"github.com/gin-gonic/gin"
+	log "github.com/sirupsen/logrus"
+)
+
+func RunWebserver(ctx context.Context, cancel context.CancelFunc, config config.AppConfig, svc domain.IValueService) {
+	controller := NewValueController(svc)
+	router := gin.Default()
+	router.GET("/index/:value", controller.GetValueIndex)
+
+	log.Printf("Starting gin server at %s", config.Port)
+	go func() {
+		err := router.Run(":" + config.Port)
+		if err != nil {
+			panic(err)
+		}
+		log.Println("Cancelling context as server exited")
+		cancel()
+	}()
+}
